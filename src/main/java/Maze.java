d6becb87104bd0d2d3f12122718b3a98a4cd3254
Max Knutsen <mknutsen99@gmail.com>
2016-04-03 16:11:38 -0400

    mazegen recursively which doesn't seem to make a solvable maze. im working on it.

--- src/main/java/Maze.java
+++ src/main/java/Maze.java
@@ -1,7 +1,10 @@
 import mknutsen.graphicslibrary.graphicsobject.RectangleGraphicObject;
 
 import java.awt.*;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 /**
  * Created by mknutsen on 4/3/16.
@@ -32,8 +35,56 @@ public class Maze extends RectangleGraphicObject {
         }
     }
 
+    public final static Maze mazeGenerator(int rows, int columns, int x, int y, int width, int height, int threshold) {
+        int[][] maze = new int[rows][columns];
+        recursiveMazeGenerator(maze, 0, 0, rows, columns, threshold);
+        return new Maze(maze, x, y, width, height);
+    }
+
+    private static int[][] recursiveMazeGenerator(int[][] maze, int x1, int y1, int x2, int y2, int threshold) {
+        //        System.out.println(String.format("%d, %d -- %d, %d -- %d", x1, y1, x2, y2, threshold));
+        if (x2 - x1 > threshold && y2 - y1 > threshold) {
+            int x = x1 + (int) (Math.random() * (x2 - x1 - 2)) + 1;
+            int y = y1 + (int) (Math.random() * (y2 - y1 - 2)) + 1;
+            for (int i = x1; i < x2; i++) {
+                maze[i][y] = 1;
+            }
+            maze[x1 + (int) (Math.random() * (x - x1 - 2)) + 1][y] = 0;
+            maze[x + (int) (Math.random() * (x2 - x - 2)) + 1][y] = 0;
+            for (int i = y1; i < y2; i++) {
+                maze[x][i] = 1;
+            }
+            maze[x][y1 + (int) (Math.random() * (y - y1 - 2)) + 1] = 0;
+            maze[x][y + (int) (Math.random() * (y2 - y - 2)) + 1] = 0;
+            recursiveMazeGenerator(maze, x1, y1, x, y, threshold);
+            recursiveMazeGenerator(maze, x, y1, x2, y, threshold);
+            recursiveMazeGenerator(maze, x1, y, x, y2, threshold);
+            recursiveMazeGenerator(maze, x, y, x2, y2, threshold);
+        }
+        return maze;
+    }
+
+    public final static Maze mazeFromTextFile(InputStream mazeStream, int x, int y, int width, int height) {
+        int[][] mazeArray;
+        Scanner scan = new Scanner(mazeStream);
+        ArrayList<String[]> strings = new ArrayList<>();
+        while (scan.hasNext()) {
+            String[] items = scan.nextLine().split(" ");
+            strings.add(items);
+        }
+        mazeArray = new int[strings.size()][strings.get(0).length];
+        for (int i = 0; i < mazeArray.length; i++) {
+            for (int j = 0; j < mazeArray[0].length; j++) {
+                mazeArray[i][j] = Integer.parseInt(strings.get(i)[j]);
+            }
+        }
+
+        scan.close();
+        return new Maze(mazeArray, x, y, width, height);
+    }
+
     @Override
-    public boolean draw(final Graphics gr) {
+    public final boolean draw(final Graphics gr) {
         walls.forEach((key, value) -> value.draw(gr));
         return true;
     }
--- src/main/java/MazeDisplay.java
+++ src/main/java/MazeDisplay.java
@@ -2,41 +2,22 @@ import mknutsen.graphicslibrary.GraphicsComponent;
 import mknutsen.graphicslibrary.GraphicsDriver;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  * Created by mknutsen on 4/3/16.
  */
 public class MazeDisplay extends GraphicsComponent {
 
-    private int[][] mazeArray;
-
     private Maze maze;
 
-    public MazeDisplay(String mazeLoc) {
-
-        Scanner scan = new Scanner(getClass().getResourceAsStream(mazeLoc));
-        ArrayList<String[]> strings = new ArrayList<>();
-        while (scan.hasNext()) {
-            String[] items = scan.nextLine().split(" ");
-            strings.add(items);
-        }
-        mazeArray = new int[strings.size()][strings.get(0).length];
-        for (int i = 0; i < mazeArray.length; i++) {
-            for (int j = 0; j < mazeArray[0].length; j++) {
-                mazeArray[i][j] = Integer.parseInt(strings.get(i)[j]);
-            }
-        }
-
-        scan.close();
-        maze = new Maze(mazeArray, 0, 0, 700, 700);
-
+    public MazeDisplay(Maze maze) {
+        this.maze = maze;
     }
 
 
     public static void main(String[] args) {
-        new GraphicsDriver(700, 700, new MazeDisplay("graph.txt"));
+        new GraphicsDriver(700, 700, new MazeDisplay(Maze.mazeGenerator(30, 30, 0, 0, 700, 700, 2)));
+        //getClass().getResourceAsStream(mazeLoc)
     }
 
     @Override
